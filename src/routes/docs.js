9f131b8b1b05c89fd96cdee45fa0973408bd06bb
EmmanuelSalinas98 <emmanuel.salinas@sib-ingenieria.com.mx>
2021-12-14 16:33:37 -0600

    Ya se registran los documents

--- src/routes/docs.js
+++ src/routes/docs.js
@@ -1,22 +1,60 @@
 const express = require('express');
 const res = require('express/lib/response');
 const router = express.Router()
-const { isAuthenticated } =require('../helpers/auth')
+const { isAuthenticated } = require('../helpers/auth')
+const client = require('../database.js')
+const { ObjectId } = require('mongodb')
 
-router.get('/docs/new', isAuthenticated,(req, res) =>{
+router.get('/docs/new', isAuthenticated, (req, res) => {
     res.render('documents/indexDocument')
 });
 
-router.get('/docs/list', isAuthenticated,(req, res) =>{
-    res.render('documents/listDocuments')
-});
- 
+router.get('/docs/list', isAuthenticated, async (req, res) => {
+    try {
+        await client.connect()
+        db = client.db('documents_nodejs')
+        collectionDocs = db.collection('docs')
+        var userid= req.user._id
 
-router.post('/docs/new', isAuthenticated,async(req,res)=>{
-    const {htmlDoc} = req.body
+        
+        
+        const docs1 = await collectionDocs.find({ idUser:userid },)
+        const docs = await docs1.toArray()
+        client.close();
+        
+        res.render('documents/listDocuments', {docs})
+
+    } catch (err) {
+        console.error(err)
+    }
+});
+
+
+router.post('/docs/new', isAuthenticated, async (req, res) => {
+    const { htmlDoc, titulo } = req.body
     const today = new Date().toUTCString()
     const user = req.user._id
 
+
+
+    try {
+        await client.connect()
+        db = client.db('documents_nodejs')
+        collectionDocs = db.collection('docs')
+
+        await collectionDocs.insertOne({ titulo: titulo, dateCreation: today, docHtml: htmlDoc, idUser: user }, function (err, res) {
+            if (err) throw err;
+            console.log('1 document inserted!');
+            client.close();
+        })
+        //req.flash('success_msg', 'Registro exitoso')
+        res.json({ status: "Success", redirect: '/docs/list' })
+
+
+
+    } catch (err) {
+        console.error(error)
+    }
 
 
 
@@ -26,7 +64,7 @@ router.post('/docs/new', isAuthenticated,async(req,res)=>{
     console.log(htmlDoc)
     console.log(today)
     console.log(user)
-    
+
 })
 
-module.exports=router;
+module.exports = router;
